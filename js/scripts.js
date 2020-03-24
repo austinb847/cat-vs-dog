facb2e7c7175678da5980743f7ddf7f9f3de3d7b
austinb847 <austinb847@icloud.com>
2020-03-24 16:47:39 -0700

    add toggle class method to change background color of page when button is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
 
   $("button#meow").click(function(){
+    $("body").removeClass();
     $("body").toggleClass("green-background");
     $("ul#cat").append("<li>MEOW!</li>");
     $("ul#dog").append("<li>WOOF WOOF BARK BARK!</li>");
   });
 
   $("button#bark").click(function(){
+    $("body").removeClass();
     $("body").toggleClass("brown-background");
     $("ul#dog").append("<li>WOOF!</li>");
     $("ul#cat").append("<li>MEOWWWWW!!!</li>");
